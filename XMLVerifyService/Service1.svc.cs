d06f575b8f4f74d231ea25f934a6b0dfc5a4861b
afmay <71290938+afmay@users.noreply.github.com>
2023-03-30 22:21:10 -0700

    Add files via upload

---
+++ XMLVerifyService/Service1.svc.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.IO;
+using System.Xml.Schema;
+using System.Xml;
+
+namespace XMLVerifyService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        List<string> validationErrors = new List<string>();
+        public string[] xml_verification(string xsd, string xml)
+        {
+            
+            try
+            {
+                XmlSchemaSet schemaSet = new XmlSchemaSet();
+                schemaSet.Add(null, xsd);
+
+                XmlReaderSettings settings = new XmlReaderSettings();
+                settings.ValidationType = ValidationType.Schema;
+                settings.Schemas = schemaSet;
+                settings.ValidationEventHandler += ValidationEventHandler;
+
+                List<string> errors = new List<string>();
+
+                using (XmlReader reader = XmlReader.Create(xml, settings))
+                {
+                    while (reader.Read()) { }
+                }
+
+                if (validationErrors.Count > 0)
+                {
+                    errors.AddRange(validationErrors);
+                }
+
+                if (errors.Count > 0)
+                {
+                    return errors.ToArray();
+                }
+                else
+                {
+                    return new string[] { "No validation errors found." };
+                }
+            }
+            catch (Exception ex)
+            {
+                return new string[] { $"Error: {ex.Message}" };
+            }
+        }
+        private void ValidationEventHandler(object sender, ValidationEventArgs e)
+        {
+            validationErrors.Add(e.Message);
+        }
+    }
+}
---
+++ XPathXMLSearchService/Service1.svc.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.IO;
+using System.Xml;
+using System.Xml.XPath;
+
+namespace XPathXMLSearchService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public string[] XPathSearch(string xml, string pathExpression)
+        {
+            try
+            {
+                List<string> results = new List<string>();
+
+                using (XmlReader reader = XmlReader.Create(xml))
+                {
+                    XPathDocument document = new XPathDocument(reader);
+                    XPathNavigator navigator = document.CreateNavigator();
+
+                    XPathNodeIterator nodeIterator = navigator.Select(pathExpression);
+
+                    while (nodeIterator.MoveNext())
+                    {
+                        results.Add(nodeIterator.Current.Value);
+                    }
+                }
+
+                if (results.Count > 0)
+                {
+                    return results.ToArray();
+                }
+                else
+                {
+                    return new string[] { "No matching nodes found." };
+                }
+            }
+            catch (Exception ex)
+            {
+                return new string[] { $"Error: {ex.Message}" };
+            }
+        }
+    }
+}
